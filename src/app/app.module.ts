9e8470df9d966e9d12eb156464de7f98043759f6
rafael176 <rafael@androitoverseas.net>
2021-11-30 16:47:22 -0700

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    username:[""],
+    password:[""]
+
+  })
+  constructor(private authSvc: AuthService,private fb :FormBuilder,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onLogin():void{
+    const formValue = this.loginForm.value;
+    this.authSvc.login(formValue).subscribe(res => {
+      if(res){
+        this.router.navigate(["home"])
+      }
+
+
+    })
+
+
+  }
+
+}
